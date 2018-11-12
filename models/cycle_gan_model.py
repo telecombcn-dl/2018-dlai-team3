2bb6017e805e72fed11d196ab23b28ff5b8bed0c
Pau Rodriguez <rodriguez.pau@gmail.com>
2018-11-12 20:57:13 +0100

    trying to run on collab, testing dirty code modifications

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -43,6 +43,10 @@ class CycleGANModel(BaseModel):
         # load/define networks
         # The naming conversion is different from those used in the paper
         # Code (paper): G_A (G), G_B (F), D_A (D_Y), D_B (D_X)
+
+        # for collab setup, dirty trick
+        self.gpu_ids2 = [str(i) for i in range(len(self.gpu_ids))]
+        print(self.gpu_ids2)
         self.netG_A = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, opt.norm,
                                         not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
         self.netG_B = networks.define_G(opt.output_nc, opt.input_nc, opt.ngf, opt.netG, opt.norm,
